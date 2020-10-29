b01f015932c9e839e11d98de13a8122604bcd599
mayanknimcet188 <205119053@nitt.edu>
2020-10-29 14:32:41 +0530

    Added config.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
 from flask import Flask
+from config import Config
 app = Flask(__name__)
-from app import routes
+app.config.from_object(Config)
+from app import routes #imported last due to cyclic dependency
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,23 @@
+from flask import render_template
 from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello,World!"
-
+    user = {'username':'Mayank'}
+    posts = [
+        {
+            'author': {'username':'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username':'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author':{'username':'Mayank'},
+            'body':'Hi this is my first post'
+        }                                      
+    ]                    
+    return render_template('index.html',title='Home', user=user, posts=posts)
+           
+           
