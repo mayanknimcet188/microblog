6f27047cc8606b10647a9a15024059afd0d69436
mayanknimcet188 <205119053@nitt.edu>
2020-10-29 21:37:12 +0530

    added database migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__)
 app.config.from_object(Config)
-from app import routes #imported last due to cyclic dependency
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models #imported last due to cyclic dependency
