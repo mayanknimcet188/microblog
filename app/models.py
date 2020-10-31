91e18615091307d60ccebf3bcaba2e0cbba1b54d
mayanknimcet188 <205119053@nitt.edu>
2020-11-01 01:22:13 +0530

    Registration page added

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from app import db
+from app import login
 from datetime import datetime
-
-class User(db.Model):
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index = True,unique=True)
     email = db.Column(db.String(128), index = True,unique = True)
@@ -9,6 +11,10 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    def set_password(self,password):
+        self.password_hash = generate_password_hash(password)
+    def check_password(self,password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -18,3 +24,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
